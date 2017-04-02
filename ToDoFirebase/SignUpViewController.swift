c578dd028f70af24add8c9aec55d273accdb28f8
morphio <a.rubtsov62@gmail.com>
2017-04-02 21:33:57 +0300

    add 	NSPhotoLibraryUsageDescription into Info.plist and  add @IBOutlets for all view in SignUpController. realize pickerControllerDelegate method for choose picture

--- ToDoFirebase/SignUpViewController.swift
+++ ToDoFirebase/SignUpViewController.swift
@@ -8,12 +8,60 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var userImageView: ProfileImageView!
+    @IBOutlet weak var usernameTextField: CustomTextField!
+    @IBOutlet weak var emailTextField: CustomTextField!
+    @IBOutlet weak var passwordTestField: CustomTextField!
+    @IBOutlet weak var countryTextField: CustomTextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    @IBAction func signUpButton(_ sender: Any) {
+        
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        self.dismiss(animated: true, completion: nil)
+        self.userImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        
+    }
 
+    @IBAction func choosePictureAction(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.allowsEditing = true
+        
+        let alertController = UIAlertController(title: "Add a Picture", message: "Choose From", preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { (action) in
+            pickerController.sourceType = .camera
+            self.present(pickerController, animated: true, completion: nil)
+            
+        }
+        
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { (action) in
+            pickerController.sourceType = .photoLibrary
+            self.present(pickerController, animated: true, completion: nil)
+            
+        }
+        
+        let savedPhotoAction = UIAlertAction(title: "Saved Photos Album", style: .default) { (action) in
+            pickerController.sourceType = .savedPhotosAlbum
+            self.present(pickerController, animated: true, completion: nil)
+            
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        
+        alertController.addAction(cameraAction)
+        alertController.addAction(photoLibraryAction)
+        alertController.addAction(savedPhotoAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
 }
