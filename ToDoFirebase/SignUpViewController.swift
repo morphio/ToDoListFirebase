433b8e2561528c468b0963c44d2e666405659f7d
morphio <a.rubtsov62@gmail.com>
2017-04-02 22:40:19 +0300

    add countryArray and picker for country in signUpController. realise UIPickerViewDelegate and DataSource,  and gesture for close pickerView

--- ToDoFirebase/SignUpViewController.swift
+++ ToDoFirebase/SignUpViewController.swift
@@ -8,29 +8,67 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class SignUpViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet weak var userImageView: ProfileImageView!
     @IBOutlet weak var usernameTextField: CustomTextField!
     @IBOutlet weak var emailTextField: CustomTextField!
     @IBOutlet weak var passwordTestField: CustomTextField!
     @IBOutlet weak var countryTextField: CustomTextField!
+    fileprivate var pickerView : UIPickerView!
+    
+    fileprivate var countryArray = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        countryArray = NSLocale.isoCountryCodes.map { (code:String) -> String in
+            let id = NSLocale.localeIdentifier(fromComponents: [NSLocale.Key.countryCode.rawValue: code])
+            return NSLocale(localeIdentifier: "en_US").displayName(forKey: NSLocale.Key.identifier, value: id) ?? "Country not found for code: \(code)"
+        }
+        pickerView = UIPickerView()
+        pickerView.delegate = self
+        countryTextField.inputView = pickerView
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(SignUpViewController.dismissController(gesture:)))
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        self.view.addGestureRecognizer(tapGestureRecognizer)
     }
-    @IBAction func signUpButton(_ sender: Any) {
-        
-        
+    
+    //MARK: - UIPickerViewDelegate -
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return countryArray[row]
     }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        countryTextField.text! = countryArray[row]
+    }
+    
+    //MARK: - UIPickerViewDataSource -
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return countryArray.count
+    }
+    //MARK: - UIImagePickerControllerDelegate -
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         self.dismiss(animated: true, completion: nil)
         self.userImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let title = NSAttributedString(string: countryArray[row], attributes: [NSForegroundColorAttributeName: UIColor.white])
+        return title
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //MARK: - Actions -
+    @IBAction func signUpButton(_ sender: Any) {
         
     }
-
+    
     @IBAction func choosePictureAction(_ sender: Any) {
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
@@ -63,5 +101,8 @@ class SignUpViewController: UIViewController, UIImagePickerControllerDelegate, U
         alertController.addAction(savedPhotoAction)
         alertController.addAction(cancelAction)
         present(alertController, animated: true, completion: nil)
+    }
+    func dismissController(gesture: UITapGestureRecognizer) -> Void {
+        self.view.endEditing(true)
     }
 }
