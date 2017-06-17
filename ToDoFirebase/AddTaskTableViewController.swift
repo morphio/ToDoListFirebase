c8f0378bf10d4bd7e6d15fd1a71803ee7498de0c
morphio <a.rubtsov62@gmail.com>
2017-06-17 14:50:56 +0300

    delete Todo.swift and add RGB colours without UIColor, refactor toAnyObject method. addTastViewController add databaseRef and method saving Task in FireBase

--- ToDoFirebase/AddTaskTableViewController.swift
+++ ToDoFirebase/AddTaskTableViewController.swift
@@ -14,6 +14,10 @@ class AddTaskTableViewController: UITableViewController {
 
     @IBOutlet weak var taskTextField: UITextField!
     @IBOutlet weak var descriptionTextView: UITextView!
+    
+    var databaseRef: FIRDatabaseReference {
+        return FIRDatabase.database().reference()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,5 +30,36 @@ class AddTaskTableViewController: UITableViewController {
     }
 
     @IBAction func saveAction(_ sender: Any) {
+        // создание ссылки на задачу
+        let taskRef = databaseRef.child("AllTasks").childByAutoId()
+        
+        var title: String!
+        if taskTextField.text == ""{
+            taskTextField.text = "No task name"
+            title = taskTextField.text
+        }else{
+            title = taskTextField.text
+
+        }
+        
+        let description:String!
+        
+        if descriptionTextView.text == ""{
+            descriptionTextView.text = "No description of this task"
+            description = descriptionTextView.text
+        }else{
+            description = descriptionTextView.text
+        }
+        
+        let red = CGFloat(arc4random_uniform(UInt32(225.5)))/255.5
+        let blue = CGFloat(arc4random_uniform(UInt32(225.5)))/255.5
+        let green = CGFloat(arc4random_uniform(UInt32(225.5)))/255.5
+        
+        
+        let task = Task(title: title, desc: description, username: FIRAuth.auth()!.currentUser!.displayName!, red: red, blue: blue, green: green)
+        
+        taskRef.setValue(task.toAnyObject())
+        
     }
+    
 }
--- ToDoFirebase/Task.swift
+++ ToDoFirebase/Task.swift
@@ -15,16 +15,20 @@ class Task:NSObject {
     var title: String!
     var desc: String! // description
     var username: String!
-    var color: UIColor
+    var red: CGFloat
+    var blue: CGFloat
+    var green: CGFloat
     var reference: FIRDatabaseReference?
     var key: String!
     
-    init(title: String, desc: String, username: String, color: UIColor, key: String = "") {
+    init(title: String, desc: String, username: String, red: CGFloat, blue: CGFloat, green: CGFloat, key: String = "") {
         
         self.title = title
         self.desc = desc
         self.username = username
-        self.color = color
+        self.red = red
+        self.blue = blue
+        self.green = green
         self.key = key
         self.reference = FIRDatabase.database().reference()
         
@@ -36,12 +40,14 @@ class Task:NSObject {
         self.title = values["title"] as! String
         self.desc = values["desc"] as! String
         self.username = values["username"] as! String
-        self.color = values["color"] as! UIColor
+        self.red = values["red"] as! CGFloat
+        self.blue = values["red"] as! CGFloat
+        self.green = values["red"] as! CGFloat
         self.key = values["key"] as! String
         self.reference = snapshot.ref
     }
     
-    func stringToAny() -> [String: Any] {
-        return ["title" : title, "desc": desc, "username": username, "color": color, "key" : key,  ]
+    func toAnyObject() -> [String: AnyObject] {
+        return ["title" : title as AnyObject, "desc": desc as AnyObject, "username": username as AnyObject, "red": red as AnyObject, "blue": blue as AnyObject, "green": green as AnyObject, "key" : key as AnyObject]
     }
 }
