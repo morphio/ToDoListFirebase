2aa37ec05360459b7068741817524b65432a9733
morphio <a.rubtsov62@gmail.com>
2017-06-21 19:15:20 +0300

    refactor task.swift

--- ToDoFirebase/Task.swift
+++ ToDoFirebase/Task.swift
@@ -37,14 +37,15 @@ struct Task {
     init(snapshot: FIRDataSnapshot){
         let values = snapshot.value as! [String: AnyObject]
 
-        self.title = values["title"] as? String
-        self.desc = values["desc"] as? String
-        self.username = values["username"] as? String
-        self.key = values["key"] as? String
+        self.key = snapshot.key
+        self.reference = snapshot.ref
+        self.title = (snapshot.value! as! NSDictionary)["title"] as? String
+        self.desc = (snapshot.value! as! NSDictionary)["desc"] as? String
+        self.username = (snapshot.value! as! NSDictionary)["username"] as? String
         self.red = values["red"] as! CGFloat
         self.blue = values["blue"] as! CGFloat
         self.green = values["green"] as! CGFloat
-        self.reference = snapshot.ref
+
     }
     
     func toAnyObject() -> [String: AnyObject] {
